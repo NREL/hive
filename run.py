4739bf271b2dede205133cc5c87ab958f9eebfb9
bborlaug <brennan.borlaug@nrel.gov>
2019-04-14 10:09:44 -0600

    Added comments outlining future work

--- run.py
+++ run.py
@@ -12,20 +12,31 @@ if __name__ == "__main__":
     today = datetime.now()
     date = "{0}-{1}-{2}".format(today.month, today.day, today.year)
 
+    print("#"*30)
+    print("Starting Simulation")
+    print("#"*30)
+    print()
+
+    print("Processing requests")
     #Combine requests files, add pax count if not exists
     reqs_df = pp.load_requests(cfg.REQUEST_PATH)
-    print(len(reqs_df))
+    print("loaded {} requests".format(len(reqs_df)))
     
     #Filter requests where distance < 0.05 miles
     reqs_df = pp.filter_short_trips(reqs_df, min_miles=0.05)
-    print(len(reqs_df))
+    print("filtered requests violating min distance req, {} remain".format(len(reqs_df)))
 
     #Filter requests where pickup/dropoff location outside operating area
     reqs_df = pp.filter_requests_outside_oper_area(reqs_df, cfg.OPERATING_AREA_PATH)
-    print(len(reqs_df))
+    print("filtered requests outside of operating area, {} remain".format(len(reqs_df)))
 
     #Pool requests - from hive.pool, module for various pooling types - o/d, dynamic, n/a
-    #pooled_reqs_df.to_csv(cfg.OUT_PATH + sim_name + 'requests/' + requests_filename, index=False)
+
+    #reqs_df.to_csv(cfg.OUT_PATH + sim_name + 'requests/' + requests_filename, index=False)
+
+    #Create output paths - 
+
+
 
 
 
