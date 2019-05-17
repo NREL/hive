ecda5712808f606c200fce43c470b5f747dd515b
Nick Reinicke <nicholas.reinicke@nrel.gov>
2019-05-17 09:43:00 -0600

    First pass at create unit tests for the tripenergy sub module.

--- tests/test_tripenergy.py
+++ tests/test_tripenergy.py
@@ -1,4 +1,14 @@
 import unittest
+import sys
+import os
+
+sys.path.append('../')
+from hive.tripenergy import import_whmi_template, create_scaled_whmi, \
+    calc_trip_kwh, calc_idle_kwh
+
+import config as cfg
+
+WHMI_LOOKUP_FILE = os.path.join('../', cfg.IN_PATH, '.lib', 'wh_mi_lookup.csv')
 
 class TripEnergyTest(unittest.TestCase):
     @classmethod
@@ -25,17 +35,24 @@ class TripEnergyTest(unittest.TestCase):
         pass
 
     def test_import_whmi_template(self):
-        pass
+        whmi_template = import_whmi_template(WHMI_LOOKUP_FILE)
+        self.assertTrue(whmi_template is not None)
 
     def test_create_scaled_whmi(self):
-        pass
+        whmi_template = import_whmi_template(WHMI_LOOKUP_FILE)
+        scaled_whmi_lookup = create_scaled_whmi(whmi_template, 0.8)
+        self.assertEqual(scaled_whmi_lookup['avg_spd_mph'][-1], 62.5)
+        self.assertAlmostEqual(scaled_whmi_lookup['whmi'][0], 1.1487803568)
 
     def test_calc_trip_kwh(self):
-        pass
+        whmi_template = import_whmi_template(WHMI_LOOKUP_FILE)
+        scaled_whmi_lookup = create_scaled_whmi(whmi_template, 0.8)
+        self.assertAlmostEqual(calc_trip_kwh(10, 3600, scaled_whmi_lookup), 0.009620097104)
 
     def test_calc_idle_kwh(self):
-        pass
+        self.assertEqual(calc_idle_kwh(3600), 0.8)
+
 
 
 if __name__ == '__main__':
-    unittest.main()  
+    unittest.main()
