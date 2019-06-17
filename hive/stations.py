eaa637d11cf57320f33df9ebc8c2980f384c9bb6
Holden, Jacob <Jacob.Holden@nrel.gov>
2019-06-17 10:36:10 -0600

    fix indent level for VehicleBase

--- hive/stations.py
+++ hive/stations.py
@@ -106,104 +106,104 @@ class FuelStation:
             self._logfile)
 
     
-    class VehicleBase:
-        """
-        Base class for fleet vehicle base (home) location. Vehicle bases are 
-        locations that inactive vehicles return to when they are not serving 
-        demand to recharge for the next peak period. Vehicles can be assigned 
-        to a base (home) location or bases can accomodate many inactive vehicles
-        (similar to a fleet depot).
+class VehicleBase:
+    """
+    Base class for fleet vehicle base (home) location. Vehicle bases are 
+    locations that inactive vehicles return to when they are not serving 
+    demand to recharge for the next peak period. Vehicles can be assigned 
+    to a base (home) location or bases can accomodate many inactive vehicles
+    (similar to a fleet depot).
 
-        Inputs
-        ------
-        base_id : int
-            Identifer assigned to VehicleBase object
-        latitude : float
-            Latitude of base location
-        longitude: float
-            Longitude of base location
-        plugs: int
-            Number of plugs at location
-        plug_type: str
-            Plug type - AC or DC
-        plug_power: float
-            Plug power in kW
-        logfile: str
-            Path to base log file
+    Inputs
+    ------
+    base_id : int
+        Identifer assigned to VehicleBase object
+    latitude : float
+        Latitude of base location
+    longitude: float
+        Longitude of base location
+    plugs: int
+        Number of plugs at location
+    plug_type: str
+        Plug type - AC or DC
+    plug_power: float
+        Plug power in kW
+    logfile: str
+        Path to base log file
 
-        Attributes
-        ----------
-        charge_cnt:
-            Number of charge events
-        instantaneous_pwr:
-            Instantaneous load in kW
-        peak_pwr:
-            Peak load in kW
-        total_energy:
-            Total energy supplied for recharging in kWh
-        avail_plugs:
-            Number of plugs that are unoccupied
-        """
+    Attributes
+    ----------
+    charge_cnt:
+        Number of charge events
+    instantaneous_pwr:
+        Instantaneous load in kW
+    peak_pwr:
+        Peak load in kW
+    total_energy:
+        Total energy supplied for recharging in kWh
+    avail_plugs:
+        Number of plugs that are unoccupied
+    """
 
-    _STATS = [
-        'charge_cnt',
-        'instantaneous_pwr',
-        'peak_pwr',
-        'total_energy'
-        ]
+_STATS = [
+    'charge_cnt',
+    'instantaneous_pwr',
+    'peak_pwr',
+    'total_energy'
+    ]
 
-    _LOG_COLUMNS = [
-        'base_id',
-        'vehicle_id',
-        'start_time',
-        'end_time',
-        'soc_i',
-        'soc_f',
-        ]
+_LOG_COLUMNS = [
+    'base_id',
+    'vehicle_id',
+    'start_time',
+    'end_time',
+    'soc_i',
+    'soc_f',
+    ]
 
-    def __init__(
-                self,
-                base_id,
-                latitude,
-                longitude,
-                plugs,
-                plug_type,
-                plug_power,
-                logfile
-                ):
+def __init__(
+            self,
+            base_id,
+            latitude,
+            longitude,
+            plugs,
+            plug_type,
+            plug_power,
+            logfile
+            ):
 
-        self.ID = base_id
-        self.LAT = latitude
-        self.LON = longitude
+    self.ID = base_id
+    self.LAT = latitude
+    self.LON = longitude
 
-        assert_constraint("TOTAL_PLUGS", plugs, STATION_PARAMS, context="Initialize FuelStation")
-        self.TOTAL_PLUGS = plugs
+    assert_constraint("TOTAL_PLUGS", plugs, STATION_PARAMS, context="Initialize FuelStation")
+    self.TOTAL_PLUGS = plugs
 
-        assert_constraint("PLUG_TYPE", plug_type, STATION_PARAMS, context="Initialize FuelStation")
-        self.PLUG_TYPE = plug_type
+    assert_constraint("PLUG_TYPE", plug_type, STATION_PARAMS, context="Initialize FuelStation")
+    self.PLUG_TYPE = plug_type
 
-        assert_constraint("PLUG_POWER", plug_power, STATION_PARAMS, context="Initialize FuelStation")
-        self.PLUG_POWER = plug_power
+    assert_constraint("PLUG_POWER", plug_power, STATION_PARAMS, context="Initialize FuelStation")
+    self.PLUG_POWER = plug_power
 
-        self.avail_plugs = plugs
+    self.avail_plugs = plugs
 
-        self._logfile = logfile
-        initialize_log(self._LOG_COLUMNS, self._logfile)
+    self._logfile = logfile
+    initialize_log(self._LOG_COLUMNS, self._logfile)
 
-        self.stats = dict()
-        for stat in self._STATS:
-            self.stats[stat] = 0
+    self.stats = dict()
+    for stat in self._STATS:
+        self.stats[stat] = 0
 
-    def add_charge_event(self, veh, start_time, end_time, soc_i, soc_f):
-        #TODO: Update
-        self.stats['charge_cnt'] += 1
-        write_log({
-            'station_id': self.ID,
-            'vehicle_id': veh.ID,
-            'start_time': start_time,
-            'end_time': end_time,
-            'soc_i': soc_i,
-            'soc_f': soc_f,
-            },
-            self._LOG_COLUMNS,
-            self._logfile)
+def add_charge_event(self, veh, start_time, end_time, soc_i, soc_f):
+    #TODO: Update
+    self.stats['charge_cnt'] += 1
+    write_log({
+        'station_id': self.ID,
+        'vehicle_id': veh.ID,
+        'start_time': start_time,
+        'end_time': end_time,
+        'soc_i': soc_i,
+        'soc_f': soc_f,
+        },
+        self._LOG_COLUMNS,
+        self._logfile)
