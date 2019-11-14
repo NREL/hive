dfece3fbee30887b2fa1297b0cbe0a1ef93c38e3
Robert Fitzgerald <Robert.Fitzgerald@nrel.gov>
2019-11-14 15:41:40 -0700

    removed unused active/available/is_valid_transition methods/data from VehicleState

--- hive/model/vehiclestate.py
+++ hive/model/vehiclestate.py
@@ -24,44 +24,10 @@ class VehicleState(Enum):
     DISPATCH_STATION = 7
     CHARGING_STATION = 8
 
-    def active(self):
-        return _state_active_properties[self]
-
-    def available(self):
-        return _state_available_properties[self]
-
     @classmethod
     def is_valid(cls, state):
         return state in _valid_vehicle_states
 
-    @classmethod
-    def is_valid_transition(cls, state_i, state_f):
-        return state_f in _valid_vehicle_transitions[state_i]
-
-
-_state_active_properties = {
-    VehicleState.DISPATCH_BASE: False,
-    VehicleState.CHARGING_BASE: False,
-    VehicleState.RESERVE_BASE: False,
-    VehicleState.IDLE: True,
-    VehicleState.REPOSITIONING: True,
-    VehicleState.DISPATCH_TRIP: True,
-    VehicleState.SERVICING_TRIP: True,
-    VehicleState.DISPATCH_STATION: True,
-    VehicleState.CHARGING_STATION: True
-}
-
-_state_available_properties = {
-    VehicleState.DISPATCH_BASE: False,
-    VehicleState.CHARGING_BASE: False,
-    VehicleState.RESERVE_BASE: False,
-    VehicleState.IDLE: True,
-    VehicleState.REPOSITIONING: True,
-    VehicleState.DISPATCH_TRIP: False,
-    VehicleState.SERVICING_TRIP: False,
-    VehicleState.DISPATCH_STATION: False,
-    VehicleState.CHARGING_STATION: False
-}
 
 _valid_vehicle_states = frozenset([
     VehicleState.DISPATCH_BASE,
