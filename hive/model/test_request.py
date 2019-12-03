1f4df4d674bb73b7837123ee601c0423c1f92565
Nick Reinicke <nicholas.reinicke@nrel.gov>
2019-12-03 14:44:54 -0700

    Updating broken tests from other modules

--- hive/model/test_request.py
+++ hive/model/test_request.py
@@ -15,10 +15,8 @@ from h3 import h3
 
 class MyTestCase(unittest.TestCase):
     request_id = "test"
-    origin = Coordinate(lat=0.0, lon=0.0)
-    destination = Coordinate(lat=3.0, lon=4.0)
-    o_geoid = h3.geo_to_h3(origin.lat, origin.lon, res=11)
-    d_geoid = h3.geo_to_h3(destination.lat, destination.lon, res=11)
+    origin = h3.geo_to_h3(0, 0, res=11)
+    destination = h3.geo_to_h3(3, 4, res=11)
     departure_time = 28800
     cancel_time = 29400
     passengers = 2
@@ -26,8 +24,6 @@ class MyTestCase(unittest.TestCase):
         request_id=request_id,
         origin=origin,
         destination=destination,
-        origin_geoid=o_geoid,
-        destination_geoid=d_geoid,
         departure_time=departure_time,
         cancel_time=cancel_time,
         passengers=passengers
@@ -51,9 +47,9 @@ class MyTestCase(unittest.TestCase):
         battery = EnergySource.build(EnergyType.ELECTRIC, 100.0)
         vehicle = Vehicle(id="test_vehicle",
                           powertrain_id="fake_powertrain_id",
-                          position=Coordinate(0, 0),
-                          battery=battery,
-                          geoid=h3.geo_to_h3(0, 0, 11))
+                          energy_source=EnergySource.build(EnergyType.ELECTRIC, 40, 1),
+                          geoid=h3.geo_to_h3(0, 0, 11),
+                          property_link=None)
         request_as_passengers = self.request.passengers
         updated_vehicle = vehicle.add_passengers(request_as_passengers)
 
@@ -69,8 +65,8 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(passenger.id, target_passenger_id)
 
             # passenger details should be consistent with request
-            self.assertEqual(passenger.origin, self.request.o_geoid)
-            self.assertEqual(passenger.destination, self.request.d_geoid)
+            self.assertEqual(passenger.origin, self.request.origin)
+            self.assertEqual(passenger.destination, self.request.destination)
             self.assertEqual(passenger.departure_time, self.request.departure_time)
 
             # the current vehicle should be known to each passenger
