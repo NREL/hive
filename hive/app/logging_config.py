94596534e71dcde78af1e9d4c03fdcf10a2a4c26
nreinick <Nicholas.Reinicke@nrel.gov>
2020-04-06 10:08:31 -0600

    updated base management logic

--- hive/app/logging_config.py
+++ hive/app/logging_config.py
@@ -8,7 +8,7 @@ LOGGING_CONFIG = {
     },
     'handlers': {
         'console': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.StreamHandler',
             'formatter': 'simple',
         }
--- hive/dispatcher/managers/base_management.py
+++ hive/dispatcher/managers/base_management.py
@@ -6,18 +6,20 @@ from typing import Tuple, NamedTuple, TYPE_CHECKING, Optional
 from hive.dispatcher.instruction.instruction_interface import instruction_to_report
 from hive.dispatcher.instruction.instructions import (
     ChargeBaseInstruction,
-    ReserveBase)
+    ReserveBaseInstruction,
+)
 from hive.dispatcher.managers.manager_interface import ManagerInterface
-from hive.model.energy.charger import Charger
-
 from hive.external.demo_base_target.temp_base_target import BaseTarget
+from hive.model.energy.charger import Charger
+from hive.state.vehicle_state import (
+    ReserveBase,
+    ChargingBase,
+)
 
 if TYPE_CHECKING:
     from hive.state.simulation_state.simulation_state import SimulationState
     from hive.dispatcher.instruction.instruction_interface import Instruction
-    from hive.model.vehicle.vehicle import Vehicle
     from hive.util.typealiases import Report
-
 
 log = logging.getLogger(__name__)
 
@@ -44,27 +46,49 @@ class BaseManagement(NamedTuple, ManagerInterface):
         instructions = ()
         reports = ()
 
-        def _should_base_charge(vehicle: Vehicle) -> bool:
-            return bool(isinstance(vehicle.vehicle_state, ReserveBase) and not
-            vehicle.energy_source.is_at_ideal_energy_limit())
+        base_charge_vehicles = [v for v in simulation_state.vehicles.values()
+                                if isinstance(v.vehicle_state, ChargingBase)]
+        reserve_vehicles = [v for v in simulation_state.vehicles.values()
+                            if isinstance(v.vehicle_state, ReserveBase)]
 
-        # find vehicles that should charge and sort them by SoC, ascending
-        base_charge_vehicles = [v for v in simulation_state.vehicles.values() if _should_base_charge(v)]
-        base_charge_vehicles.sort(key=lambda v: v.energy_source.soc)
+        n_charging = len(base_charge_vehicles)
 
         target = self.base_target.get_target(simulation_state.sim_time)
 
-        # assign as many of these vehicles to charge as possible
-        for veh in base_charge_vehicles:
-            if len(instructions) >= target:
-                break
-            base_id = simulation_state.b_locations[veh.geoid]
-            base = simulation_state.bases[base_id]
-            if base.station_id:
-                instruction = ChargeBaseInstruction(
+        if target > n_charging:
+            # we want more vehicles charging
+            # assign as many of these vehicles to charge as possible, sorted by SOC ascending
+            reserve_vehicles.sort(key=lambda v: v.energy_source.soc)
+            diff = target - n_charging
+            for veh in reserve_vehicles:
+                if len(instructions) >= diff:
+                    break
+                base_id = simulation_state.b_locations[veh.geoid]
+                base = simulation_state.bases[base_id]
+                if base.station_id:
+                    instruction = ChargeBaseInstruction(
+                        vehicle_id=veh.id,
+                        base_id=base.id,
+                        charger=Charger.LEVEL_2,
+                    )
+
+                    report = instruction_to_report(instruction, simulation_state.sim_time)
+                    reports = reports + (report,)
+
+                    instructions = instructions + (instruction,)
+        elif target < n_charging:
+            # we have too many vehicles charging
+            # instruct the extra vehicles to reserve, sorted by SOC descending
+            base_charge_vehicles.sort(key=lambda v: v.energy_source.soc, reverse=True)
+            diff = n_charging - target
+            for veh in base_charge_vehicles:
+                if len(instructions) >= diff:
+                    break
+
+                base_id = simulation_state.b_locations[veh.geoid]
+                instruction = ReserveBaseInstruction(
                     vehicle_id=veh.id,
-                    base_id=base.id,
-                    charger=Charger.LEVEL_2,
+                    base_id=base_id,
                 )
 
                 report = instruction_to_report(instruction, simulation_state.sim_time)
