071b0886e2429b10c1a69abf2f42b8a86b2998e7
nreinick <Nicholas.Reinicke@nrel.gov>
2020-10-23 14:18:24 -0600

    remove timezones

--- hive/util/parsers.py
+++ hive/util/parsers.py
@@ -8,12 +8,16 @@ from hive.util.typealiases import SimTime
 
 def time_parser(value: str) -> Union[IOError, SimTime]:
     try:
-        time = int(datetime.fromisoformat(value).timestamp())
+        time = datetime.fromisoformat(value)
     except (ValueError, TypeError):
         try:
-            time = int(value)
-        except ValueError:
-            return IOError(
-                "Unable to parse datetime. \
-                Make sure the time is either a unix time integer or an ISO 8601 string")
-    return time
+            time = datetime.fromtimestamp(int(value))
+        except (ValueError, TypeError):
+            return IOError("Unable to parse datetime. Make sure the time is an ISO 8601 string or an epoch integer")
+
+    if time.tzinfo:
+        return NotImplemented("Timezones are not supported in hive. Please remove any timezone info")
+
+    return int(time.timestamp())
+
+
