c2eb9d93f4f23fcba60401ba246f297305259fdb
nreinick <Nicholas.Reinicke@nrel.gov>
2021-03-17 10:23:51 -0600

    move reporter out of initialize_simulation functions

--- hive/initialization/load.py
+++ hive/initialization/load.py
@@ -6,6 +6,11 @@ from typing import Tuple
 import yaml
 
 from hive.config import HiveConfig
+from hive.reporting.handler.eventful_handler import EventfulHandler
+from hive.reporting.handler.instruction_handler import InstructionHandler
+from hive.reporting.handler.stateful_handler import StatefulHandler
+from hive.reporting.handler.stats_handler import StatsHandler
+from hive.reporting.reporter import Reporter
 from hive.runner.environment import Environment
 from hive.initialization.initialize_simulation import initialize_simulation
 from hive.state.simulation_state.simulation_state import SimulationState
@@ -18,6 +23,7 @@ def load_simulation(scenario_file_path: Path) -> Tuple[SimulationState, Environm
     takes a scenario path and attempts to build all assets required to run a scenario
 
     :param scenario_file_path: the path to the scenario file we are using
+
     :return: the assets required to run a scenario
     :raises: Exception if the scenario_path is not found or if other scenario files are not found or fail to parse
     """
@@ -35,4 +41,17 @@ def load_simulation(scenario_file_path: Path) -> Tuple[SimulationState, Environm
 
     simulation_state, environment = initialize_simulation(config)
 
-    return simulation_state, environment
+    # configure reporting
+    reporter = Reporter()
+    if config.global_config.log_events:
+        reporter.add_handler(EventfulHandler(config.global_config, config.scenario_output_directory))
+    if config.global_config.log_states:
+        reporter.add_handler(StatefulHandler(config.global_config, config.scenario_output_directory))
+    if config.global_config.log_instructions:
+        reporter.add_handler(InstructionHandler(config.global_config, config.scenario_output_directory))
+    if config.global_config.log_stats:
+        reporter.add_handler(StatsHandler())
+
+    environment_w_reporter = environment.set_reporter(reporter)
+
+    return simulation_state, environment_w_reporter
