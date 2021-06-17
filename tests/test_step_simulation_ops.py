357b0c38836cce86a0dcbdd3ecfe7450db963deb
nreinick <Nicholas.Reinicke@nrel.gov>
2021-06-17 11:32:56 -0600

    add state transition test for step vehicle

--- tests/test_step_simulation_ops.py
+++ tests/test_step_simulation_ops.py
@@ -28,3 +28,33 @@ class TestStepSimulationOps(TestCase):
 
         self.assertEqual(veh1_idle_time, 0, "vehicle 1 should not have idled")
         self.assertEqual(veh2_idle_time, 600, "vehicle 2 should have idled for 10 time steps (600 s)")
+
+    def test_step_vehicle_state_change(self):
+        """
+        build a sim with two charging vehicles and only step one of them until done charging.
+
+        check to make sure only one vehicle transitioned from the charging state to the idle state.
+        """
+        station = mock_station("s1")
+        vehicle1 = mock_vehicle(
+            vehicle_id="1",
+            vehicle_state=ChargingStation("1", "s1", mock_dcfc_charger_id()),
+        )
+        vehicle2 = mock_vehicle(
+            vehicle_id="2",
+            vehicle_state=ChargingStation("2", "s1", mock_dcfc_charger_id()),
+        )
+        sim = mock_sim(vehicles=(vehicle1, vehicle2), stations=(station,))
+        env = mock_env()
+
+        for _ in range(10):
+            sim = step_vehicle(sim, env, vehicle_id="2")
+
+        vehicle1: Vehicle = sim.vehicles.get("1")
+        vehicle2: Vehicle = sim.vehicles.get("2")
+
+        veh1_state = vehicle1.vehicle_state
+        veh2_state = vehicle2.vehicle_state
+
+        self.assertIsInstance(veh1_state, ChargingStation, "vehicle 1 should still be in charging state")
+        self.assertIsInstance(veh2_state, Idle, "vehicle 2 should have transitioned to idle")
