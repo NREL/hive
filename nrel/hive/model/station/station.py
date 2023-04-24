9204861d60b26c349c53df0af90f459f2e138577
Joshua Hoshiko <66644385+jhoshiko@users.noreply.github.com>
2023-04-24 17:13:29 -0600

    Update station.py
    Wrapped dictionary
    
    Signed-off-by: Joshua Hoshiko <66644385+jhoshiko@users.noreply.github.com>

--- nrel/hive/model/station/station.py
+++ nrel/hive/model/station/station.py
@@ -420,10 +420,10 @@ class Station(Entity):
         :param delta_energy: the energy dispensed for a charge event
         :return: the updated Station
         """
-        energy_dispensed = {
+        energy_dispensed = immutables.Map({
             k: self.energy_dispensed[k] + delta_energy.get(k, 0)
             for k in self.energy_dispensed.keys()
-        }
+        })
         return replace(self, energy_dispensed=energy_dispensed)
 
     def enqueue_for_charger(self, charger_id: ChargerId) -> ErrorOr[Station]:
