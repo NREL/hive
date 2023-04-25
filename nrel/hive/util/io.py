9399c003c86eb69270fa51f0eb277dcf1042ab75
Matt Boyd <mboyd@lexmachina.com>
2023-04-25 08:50:37 -0600

    [#164] Use Sequence instead of list for type hint

--- nrel/hive/util/io.py
+++ nrel/hive/util/io.py
@@ -1,9 +1,10 @@
 import csv
 import pathlib
+from collections.abc import Sequence
 from typing import Union
 
 
-def to_csv(data: list[Union[dict, list]], path: pathlib.Path):
+def to_csv(data: Sequence[Union[dict, list]], path: pathlib.Path):
     with open(path, 'w') as f:
         if all(isinstance(row, dict) for row in data):
             writer = csv.DictWriter(f, fieldnames=[header for header in data[0].keys()])
